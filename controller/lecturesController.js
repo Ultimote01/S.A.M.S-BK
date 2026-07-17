1784dee22a3eee86edeb58930e20383a9dbb0d99
Nathaniel <ultimote01@hotmail.com>
2026-07-17 09:47:55 +0100

    Alomost done

--- controller/lecturesController.js
+++ controller/lecturesController.js
@@ -9,7 +9,8 @@ exports.createLecture = catchAsync(async (req, res, next)=> {
 
     const modifiedRequestBody = {};
 
-   console.log( new Date(req.body.createdAt) ,  new Date(convertDateNowToUTC()))
+   console.log( new Date(req.body.createdAt) ,  new Date(convertDateNowToUTC()),
+    req.body)
 
     ['mode','startTime','endTime','createdAt','course'].forEach((el)=> {
         if (Object.keys(req.body).indexOf(el) === -1 )
